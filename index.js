76d62344413aabdee4933286b96db517602e25c9
Venkata Krishna Bharadwaz Kopalle <5936702+vkbharadwazkopalle@users.noreply.github.com>
2022-08-07 13:25:59 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,47 @@
+import './style.css';
+import { CSSHelper } from './helpers';
+
+const container = document.createElement('div');
+container.setAttribute('class', 'flex-wrap container');
+
+const colors = [];
+
+for (let i = 0; i < 10000; i++) {
+  let textColor = '';
+
+  const cssHelper = new CSSHelper(i);
+
+  colors.push({
+    red: cssHelper.randomNumberwithInRange(i * 5),
+    green: cssHelper.randomNumberwithInRange(i * 10),
+    blue: cssHelper.randomNumberwithInRange(i * 20),
+  });
+
+  if (colors[i].red >= 124 || colors[i].green >= 124) {
+    textColor = '#000';
+  } else {
+    textColor = '#fff';
+  }
+
+  let div = document.createElement('div');
+
+  div.setAttribute(
+    'style',
+    `background-color: rgb(
+      ${colors[i].red}, ${colors[i].green}, ${colors[i].blue}
+    ); 
+    color: ${textColor};`
+  );
+
+  div.setAttribute('class', 'cell');
+  const subScript = document.createElement('sub');
+  subScript.innerText = `${i}`;
+
+  div.prepend(subScript);
+
+  div.innerText = `rgb(${colors[i].red}, ${colors[i].green}, ${colors[i].blue})`;
+
+  container.appendChild(div);
+}
+
+document.body.append(container);
